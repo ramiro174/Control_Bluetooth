081944be3e7c855ea5a9dffdcbd87b42ff43fa19
Ramiro <ramiro174@gmail.com>
2022-11-24 20:45:51 -0600

    Initial commit

---
+++ app/src/main/java/com/ramiro174/controlbluetooth/Sensores/Bluetooth.java
@@ -0,0 +1,29 @@
+package com.ramiro174.controlbluetooth.Sensores;
+
+import static android.app.Activity.RESULT_OK;
+import static android.provider.Settings.System.getString;
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+
+import com.ramiro174.controlbluetooth.R;
+
+public class Bluetooth{
+
+
+
+
+
+
+}
